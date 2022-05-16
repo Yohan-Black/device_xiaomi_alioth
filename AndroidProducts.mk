dc21a883a167d5d3b28eddfe2c68e5bbd3d4092d
drishal <drishalballaney@gmail.com>
2022-05-16 19:41:53 +0530

    Alioth: optimize for aicp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_alioth.mk
+    $(LOCAL_DIR)/aicp_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_alioth-user \
-    xtended_alioth-userdebug \
-    xtended_alioth-eng
+    aicp_alioth-user \
+    aicp_alioth-userdebug \
+    aicp_alioth-eng
